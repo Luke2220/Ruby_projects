6775b8bbe9de1adbc408096a3659113c97bfd621
Luke <kingluke2222@live.com>
2020-11-01 15:47:18 -0800

    finished merge_sort

--- recursion_projects/merge_sort.rb
+++ recursion_projects/merge_sort.rb
@@ -9,17 +9,36 @@ def merge_sort(array)
     result = []
     x.each do |x_val|
         y.each do |y_val|
-            if x_val < y_val
-                result << x_val if result.include?(x_val) == false
-            else
-                result << y_val if result.include?(y_val) == false
-            end        
+            if x_val > y_val
+                if result.include?(x_val) == false    
+                result << y_val         
+                end
+            elsif x_val < y_val
+                if result.include?(y_val) == false   
+                result << x_val
+                break
+                end
+            elsif x_val == y_val
+                    result << x_val
+                    result << y_val
+                    break
+                end
            end
+
+    result.each do |val|
+        if y.index(val) != nil
+         y.delete_at(y.index(val))
         end
     end
-    result += (x + y)
-    result
-
+end
+result.each do |val| 
+    if x.index(val) != nil
+    x.delete_at(x.index(val))
+    end
 end
 
-p merge_sort([8,7,6,5,4,3,2,1])
+   p result += x + y
+    result
+end
+
+merge_sort([999,9,7,8,5,55,6,6,2,1,4,9])

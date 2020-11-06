7261df45b0c7c308f8a89c4b9d76c17e4021f137
Luke <kingluke2222@live.com>
2020-11-06 14:36:44 -0800

    completed linked_list project

--- linked_list.rb
+++ linked_list.rb
@@ -6,24 +6,26 @@ def initialize
     @head_node = nil
     @tail_node = nil
 end
+
     def append(value)
         new_node = Node.new
         new_node.value = value
-        if @tail_node != nil
-            @tail_node.next_node = tail_node
-            @tail_node = new_node
-        else
-            @tail_node = new_node
-        end
+      
+        last_node = at(size())
+        last_node.next_node = new_node
+        @tail_node = new_node
     end
+
     def prepend(value)
         new_node = Node.new
         new_node.value = value
         if @head_node != nil
-            @head_node.next_node = head_node
+            temp = head_node
             @head_node = new_node
+            @head_node.next_node = temp
         else
             @head_node = new_node
+            @head_node.next_node = @tail_node
         end
     end
 
@@ -41,17 +43,25 @@ end
         counter = 0
         current_node = @head_node
         while counter < index
-            counter++
+            break if current_node.next_node == nil
+            counter += 1
             current_node = current_node.next_node
         end
         return current_node
     end
 
     def pop
-        last_node = @tail_node
-        @tail_node = nil
-        @tail_node = at(size())
-        last_node      
+        if (size() == 0)
+            last_node = @tail_node
+            @head_node = nil
+            @tail_node = nil
+            return last_node
+        else
+            last_node = @tail_node
+            @tail_node = at(size() - 1)
+            @tail_node.next_node = nil
+            return last_node  
+        end         
     end
 
     def contains?(value)
@@ -60,20 +70,20 @@ end
             if current_node.value == value
                 return true
             end
-            current_node = current_node.next    
+            current_node = current_node.next_node
         end
         return false
     end
 
     def find (value)
         index = 0
-        current_node = @head_node
+        current_node  = @head_node
         while current_node != nil
-            index += 1
             if current_node.value == value
                 return index
             end
-            current_node = current_node.next    
+            index += 1
+            current_node = current_node.next_node  
         end
         return nil
     end
@@ -83,13 +93,47 @@ end
         string_list = ''
         current_node = @head_node
         while current_node != nil
-            current_node = current_node.next_node
-            string_list += "( #{current_node.value} => )"
+
+            string_list += "( #{current_node.value} => )" 
+            current_node = current_node.next_node           
         end
         return string_list
     end
 
+def insert_at(value,index)
 
+    counter = 0
+    current_node = @head_node
+    while counter < index - 1
+        break if current_node.next_node == nil
+        current_node = current_node.next_node
+        counter += 1
+    end
+
+    new_node = Node.new
+    new_node.value = value
+
+    temp = current_node.next_node
+    current_node.next_node = new_node
+    new_node.next_node = temp
+
+end
+
+def remove_at(index)
+  
+    index = size() if index > size()
+
+    current_node = at(index)
+    previous_node = at(index - 1)
+    if index == 0
+        @head_node = @head_node.next_node
+    elsif (current_node.next_node != nil)
+        previous_node.next_node = current_node.next_node
+    else
+        previous_node.next_node = nil
+    end
+
+end
 
 end
 
@@ -101,7 +145,5 @@ attr_accessor :value
         @value = nil
     end
 
-    def value
 
-    end
 end

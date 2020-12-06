d2f3cb340c50274611994a8156c95b842e841f2c
Luke <kingluke2222@live.com>
2020-12-06 15:43:42 -0800

    finished connect four and added more testing

--- connect_four/lib/connect_four.rb
+++ connect_four/lib/connect_four.rb
@@ -1,25 +1,21 @@
 class Board
-
+    attr_reader :slots
     def initialize()
-        @slots = Array.new(2,Array.new())
+        @slots = Array.new(7,Array.new())
 
         for i in 1..6 do
-            @slots[0] << BoardPiece.new()
+            @slots[0] << BoardSlot.new()
         end
-
-        for j in 1..7 do
-            @slots[1] << BoardPiece.new()     
-        end
-        
     end
 
-    def drop_piece(x)
+    def drop_piece(x, color)
         counter = 0
         while @slots[x.to_i][counter].is_state_empty?
             counter += 1
             return nil if counter >= 7
         end
-        @slots[x.to_i][counter].state = "blue"
+        @slots[x.to_i][counter].state = color
+        connect_slots()
     end
 
     def puts_board
@@ -29,15 +25,41 @@ class Board
         end
        end
 
+       def connect_slots
+        @slots.each do |row| 
+         row.each {|column| column.assign_neighbors(row,column,@slots)}      
+        end
+       end
+
 end
 
-class BoardPiece
-    attr_accessor :state
+class BoardSlot
+    attr_accessor :state, :neighbors
     def initialize
-        state = 'empty'
+        @state = 'empty'
+        @neighbors = Hash.new
     end
+
+    def assign_neighbors(x,y,board_array)
+        @neighbors[:right] = board_array[x+1][y] if (board_array[x+1][y].state != 'empty')
+            
+        @neighbors[:upperright] = board_array[x+1][y+1] if (board_array[x+1][y+1].state != 'empty')
+            
+        @neighbors[:up] = board_array[x][y+1] if (board_array[x][y+1].state != 'empty')
+            
+        @neighbors[:upperleft] = board_array[x-1][y+1] if (board_array[x-1][y+1].state != 'empty')
+           
+        @neighbors[:left] = board_array[x-1][y] if (board_array[x-1][y].state != 'empty')
+            
+        @neighbors[:lowerleft] = board_array[x-1][y-1] if (board_array[x-1][y-1].state != 'empty')
+            
+        @neighbors[:down] = board_array[x][y-1] if (board_array[x][y-1].state != 'empty')
+            
+        @neighbors[:lowerright] = board_array[x+1][y-1] if (board_array[x+1][y-1].state != 'empty')          
+    end
+
     def is_state_empty?()
-        return true if state == 'empty'
+        return true if @state == 'empty'
 
         return false
     end
@@ -47,26 +69,77 @@ class PlayGame
     def initialize()
        @our_board = Board.new()
        @current_player = 1
-       @symbol = 'blue'
+       @player_color = 'blue'
     end
 
     def play_turn()
         puts "Choose from columns 1-7 to play into"
         input = gets.chomp
 
-        @our_board.drop_piece(input)
+        @our_board.drop_piece(input,@player_color)
         change_player()
     end
 
     def change_player()
         if @current_player == 1
             @current_player = 2
-            @symbol = 'red'
+            @player_color = 'red'
         elsif @current_player == 2
             @current_player = 1
-            @symbol = 'blue'
+            @player_color = 'blue'
         end
     end
 
+    def check_victory()
+        @our_board.@slots.each do |row| 
+            row.each do |slot| 
+                if slot.state != 'empty'
+                    temp = slot
+                    counter = 0
+                    while temp.neighbors[:up].state != 'empty'
+                        temp = temp.neighbors[:up]
+                        counter += 1
+                        return true if counter >= 4
+                    end
+                    while temp.neighbors[:upperright].state != 'empty'
+                        temp = temp.neighbors[:upperright]
+                        counter += 1
+                        return true if counter >= 4
+                    end
+                    while temp.neighbors[:upperleft].state != 'empty'
+                        temp = temp.neighbors[:upperleft]
+                        counter += 1
+                        return true if counter >= 4
+                    end
+                    while temp.neighbors[:left].state != 'empty'
+                        temp = temp.neighbors[:left]
+                        counter += 1
+                        return true if counter >= 4
+                    end
+                    while temp.neighbors[:lowerleft].state != 'empty'
+                        temp = temp.neighbors[:lowerleft]
+                        counter += 1
+                        return true if counter >= 4
+                    end
+                    while temp.neighbors[:down].state != 'empty'
+                        temp = temp.neighbors[:down]
+                        counter += 1
+                        return true if counter >= 4
+                    end
+                    while temp.neighbors[:lowerright].state != 'empty'
+                        temp = temp.neighbors[:lowerright]
+                        counter += 1
+                        return true if counter >= 4
+                    end
+                    while temp.neighbors[:right].state != 'empty'
+                        temp = temp.neighbors[:right]
+                        counter += 1
+                        return true if counter >= 4
+                    end
+                end
+            end
+           end
+           return false
+    end
 end
 new_game = PlayGame.new
--- connect_four/spec/conect_four_spec.rb
+++ connect_four/spec/conect_four_spec.rb
@@ -1,14 +1,30 @@
 require "./lib/connect_four.rb"
 
-describe PlayGame do
-    describe "check_victory" do
-        it "has either player won the game" do
-            expect(check_victory()).to eql(true)
+describe Board do
+    descibe "drop_piece" do
+        it "pieces can be placed on board" do
+            board= Board.new()
+            board.drop_piece(1,'blue')
+            expect(board.puts_board()).to eql()
         end
+    end
+end
+
+describe PlayGame do
+    new_game = PlayGame.new
+    for i in 1..4
+        new_game.play_turn
     end
     describe "play_turn" do
         it "game pieces get placed where they are specificed" do
             expect(play_turn()).to eql(x,y)
         end
     end
+
+    describe "check_victory" do
+        it "has either player won the game" do
+            expect(check_victory()).to eql(true)
+        end
+    end
+    
 end
